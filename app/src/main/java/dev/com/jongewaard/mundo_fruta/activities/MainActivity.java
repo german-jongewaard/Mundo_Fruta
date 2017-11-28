b23d73acdbe04a2871fa2a733b94df7d439161e5
german <germanjongewaard@gmail.com>
2017-11-28 10:12:30 +0100

    New files, like always!

--- app/src/main/java/dev/com/jongewaard/mundo_fruta/activities/MainActivity.java
+++ app/src/main/java/dev/com/jongewaard/mundo_fruta/activities/MainActivity.java
@@ -17,8 +17,8 @@ import dev.com.jongewaard.mundo_fruta.models.Fruit;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
     private FruitAdapter adapter;
 
     private List<Fruit> fruits;
@@ -31,15 +31,11 @@ public class MainActivity extends AppCompatActivity {
 
         fruits = this.getAllFruits();
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        mLayoutManager = new LinearLayoutManager(this);
-        /*
-        *   Observa como pasamos el activity , con this. Podríamos declarar
-        *   Activity o Context en el constructory funcionaria pasando el mismo valor, this.
-        *
-        * */
-
-        adapter = new FruitAdapter(fruits, R.layout.recycler_view_item, this, new FruitAdapter.OnItemClickListener() {
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(this);
+        // Observa como pasamos el activity, con this. Podríamos declarar
+        // Activity o Context en el constructor y funcionaría pasando el mismo valor, this
+        adapter = new FruitAdapter(fruits, R.layout.recycler_view_fruit_item, this, new FruitAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(Fruit fruit, int position) {
                 fruit.addQuantity(1);
@@ -47,14 +43,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Lo usamos en caso de que sepamos que el layout no va a cambiar de tamaño, mejorando la performance
-        mRecyclerView.setHasFixedSize(true);
-        // Enlazamos el layout manager y adaptor directamente al recycler view
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(adapter);
-        /* No registramos para el menu contexto nada aqui, lo movemos al ViewHolder del adaptador
-        * */
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+        // No registramos para el menu contexto nada aquí, lo movemos al ViewHolder del adaptador
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -64,21 +58,22 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         switch (item.getItemId()) {
             case R.id.add_fruit:
-                //Rescatamos el numero de frutas para saber en que posición insertamos
-                int position =fruits.size();//devuelve el numero de frutas!
-                fruits.add(position, new Fruit("Plum" + (++counter), "Fruit added by the user", R.drawable.plum_bg, R.mipmap.ic_plum, 0));
+                // Rescatamos el número de frutas para saber en que posición insertaremos
+                int position = fruits.size();
+                fruits.add(position, new Fruit("Plum " + (++counter), "Fruit added by the user", R.drawable.plum_bg, R.mipmap.ic_plum, 0));
                 adapter.notifyItemInserted(position);
-                mLayoutManager.scrollToPosition(position);
+                layoutManager.scrollToPosition(position);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    private List<Fruit> getAllFruits(){
-        return new ArrayList<Fruit>(){{
+    private List<Fruit> getAllFruits() {
+        return new ArrayList<Fruit>() {{
             add(new Fruit("Strawberry", "Strawberry description", R.drawable.strawberry_bg, R.mipmap.ic_strawberry, 0));
             add(new Fruit("Orange", "Orange description", R.drawable.orange_bg, R.mipmap.ic_orange, 0));
             add(new Fruit("Apple", "Apple description", R.drawable.apple_bg, R.mipmap.ic_apple, 0));
@@ -86,22 +81,6 @@ public class MainActivity extends AppCompatActivity {
             add(new Fruit("Cherry", "Cherry description", R.drawable.cherry_bg, R.mipmap.ic_cherry, 0));
             add(new Fruit("Pear", "Pear description", R.drawable.pear_bg, R.mipmap.ic_pear, 0));
             add(new Fruit("Raspberry", "Raspberry description", R.drawable.raspberry_bg, R.mipmap.ic_raspberry, 0));
-
         }};
-    }
-
-    private void addFruit(int position) {
-        fruits.add(position, new Fruit("New fruit " + (++counter), R.drawable.plum_bg));
-        // Notificamos de un nuevo item insertado en nuestra colección
-        adapter.notifyItemInserted(position);
-        // Hacemos scroll hacia lo posición donde el nuevo elemento se aloja
-        mLayoutManager.scrollToPosition(position);
-    }
-
-    private void deleteFruit(int position){
-
-        fruits.remove(position);
-        // Notificamos de un item borrado en nuestra colección
-        adapter.notifyItemRemoved(position);
     }
 }
--- app/src/main/java/dev/com/jongewaard/mundo_fruta/models/Fruit.java
+++ app/src/main/java/dev/com/jongewaard/mundo_fruta/models/Fruit.java
@@ -7,60 +7,75 @@ package dev.com.jongewaard.mundo_fruta.models;
 public class Fruit {
 
     private String name;
-    private String descripcion;
+    private String description;
     private int imgBackground;
     private int imgIcon;
     private int quantity;
 
-    //Valores accesibles estaticamente.
+    // Valores accesibles estáticamente
     public static final int LIMIT_QUANTITY = 10;
     public static final int RESET_VALUE_QUANTITY = 0;
 
-    public Fruit(){
+    public Fruit() {
     }
 
-    public Fruit(String name, String descripcion, int imgBackground, int imgIcon, int quantity){
-
+    public Fruit(String name, String description, int imgBackground, int imgIcon, int quantity) {
         this.name = name;
-        this.descripcion = descripcion;
+        this.description = description;
         this.imgBackground = imgBackground;
         this.imgIcon = imgIcon;
         this.quantity = quantity;
     }
 
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
-    public void setName(String name) { this.name = name; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getDescripcion() { return descripcion; }
+    public String getDescription() {
+        return description;
+    }
 
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-    public int getImgBackground() { return imgBackground; }
+    public int getImgBackground() {
+        return imgBackground;
+    }
 
-    public void setImgBackground(int imgBackground) { this.imgBackground = imgBackground; }
+    public void setImgBackground(int imgBackground) {
+        this.imgBackground = imgBackground;
+    }
 
-    public int getImgIcon() { return imgIcon; }
+    public int getImgIcon() {
+        return imgIcon;
+    }
 
-    public void setImgIcon(int imgIcon) { this.imgIcon = imgIcon; }
+    public void setImgIcon(int imgIcon) {
+        this.imgIcon = imgIcon;
+    }
 
-    public int getQuantity() { return quantity; }
+    public int getQuantity() {
+        return quantity;
+    }
 
-    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
-    public static int getLimitQuantity() { return LIMIT_QUANTITY; }
-
-    public static int getResetValueQuantity() { return RESET_VALUE_QUANTITY; }
-
-    //Añadir cantidad
-    public void addQuantity(int quantity){
-
-        if(this.quantity < LIMIT_QUANTITY)
-
+    // Añadir cantidad
+    public void addQuantity(int quantity) {
+        if (this.quantity < LIMIT_QUANTITY)
             this.quantity += quantity;
     }
 
-    //Reset cantidad
-    public void resetQuantity(){ this.quantity = RESET_VALUE_QUANTITY; }
-
+    // Reset cantidad
+    public void resetQuantity() {
+        this.quantity = RESET_VALUE_QUANTITY;
+    }
 }
+
